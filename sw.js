e167e6ea6d9ff41759bb306ba4712f5c77af134b
LaminarDynamics <LaminarDynamics@gmail.com>
2023-06-27 02:42:01 -0400

    Changed Service Worker

--- sw.js
+++ sw.js
@@ -1,30 +1,41 @@
-var filesToAdd = [
-    "./src/bible_kjv.json",
-    "./src/index_mobile.css",
-    "./src/index.css",
-    "./src/index.js",
-    "./src/jquery.js",
-    "./src/style.js",
-    "./src/verse_selector.js",
-    "./index.html",
-    "./manifest.json",
-    "./sw.js"];
+let cache_name = "my_cache_v1.0";
 
-self.addEventListener("install", e => { // When service worker installed
-    console.log("Installed");
-    e.waitUntil(
-        caches.open("static").then(cache => { // Promise to settle-Get stuff to cache
-            return cache.addAll(filesToAdd);
-            // return cache.addAll(["./", "./src/index.css", "./images/logo192.png", "./index.html", "./src/bible_kjv.json"]);
-        })
-    )
+let filesToAdd = [
+    "/",
+    "sw.js",
+    "index.html",
+    "src/bible_kjv.json",
+    "src/index_mobile.css",
+    "src/index.css",
+    "src/index.js",
+    "src/jquery.js",
+    "src/style.js",
+    "src/verse_selector.js",
+    "manifest.json",
+    ];
+
+
+// Install Service Worker
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil((async () => {
+        const cache = await caches.open(cache_name);
+        console.log('[Service Worker] Caching all: app shell and content');
+        await cache.addAll(filesToAdd);
+    })());
 });
 
-self.addEventListener("fetch", e => {
-    console.log("Intercepting fetch request for : " + e.request.url);
-    e.respondWith(
-        caches.match(e.request).then(response => { // Check if items availible in cache before resorting to network
-            return response || fetch(e.request); // response = cached ELSE fetch from network
-        })
-    );
-})
+
+//  Fetch request
+self.addEventListener('fetch', (e) => { // Fires when resourse is grabbed from local. If not in local adds needed file to local after download
+    e.respondWith((async () => {
+        const r = await caches.match(e.request);
+        console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+        if (r) { return r; }
+        const response = await fetch(e.request);
+        const cache = await caches.open(cache_name);
+        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+        cache.put(e.request, response.clone());
+        return response;
+    })());
+});
